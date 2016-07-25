5f61f10c0bd068c3f85e28c4862aff2ba1555c13
sammosummo <samuel.mathias@gmail.com>
2016-07-25 08:36:18 -0400

    Initial commit (to test pypi).

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = 'loocius',
+    version = '0.1',
+    description = 'A skeleton for creating psychological tests in Python',
+    url = 'http://github.com/sammosummo/loocius',
+    author = 'Sam Mathias',
+    author_email = 'samuel.mathias@gmail.com',
+    license = 'MIT',
+    packages = ['loocius'],
+    zip_safe = False
+)
